a0bbd666340b2cd70bd1a7f3d6aaefc27c3d5f0e
David Choqueluque <dchoqueluque@gmail.com>
2022-03-13 11:51:50 -0500

    edited rwf dataset script

--- scripts/rwf-2000_dataset.sh
+++ scripts/rwf-2000_dataset.sh
@@ -1,11 +1,5 @@
 
 #!/bin/bash
-#Creating Folders
-cd /content
-echo "===> Creating folders..."
-mkdir DATASETS/
-mkdir DATASETS/ActionTubesV2
-mkdir DATASETS/Pretrained_Models
 
 #RWF dataset
 echo "===> Preparing RWF-2000 dataset..."
@@ -23,9 +17,6 @@ cp -r "$src" $dst
 unzip -q $f_name -d $dst
 rm $f_name
 
-#Update wget
-pip install --upgrade --no-cache-dir gdown
-
 #Pretrained models
 
 #Restore training
@@ -36,11 +27,3 @@ src="/content/$(ls -t | head -1)"
 dst="/content/DATASETS/Pretrained_Models"
 echo "===> Moving ${src} to ${dst}"
 mv $src ${dst}
-
-#Installing yacs
-echo "===> Installing yacs"
-pip3 install yacs
-
-#Installing CUDA
-echo "===> Installing CUDA"
-pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
